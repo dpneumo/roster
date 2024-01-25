a64d712b9a2b569db7757d1e3ecebd8de01ad2d9
dpneumo <dpneumo@gmail.com>
2024-01-24 16:47:04 -0800

    Aphone returns a phone number

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -7,4 +7,8 @@ class Phone < ApplicationRecord
   validates :area, presence: true
   validates :prefix, presence: true
   validates :number, presence: true
+
+  def ph_number
+    "(#{area}) #{prefix}-#{number}"
+  end
 end
